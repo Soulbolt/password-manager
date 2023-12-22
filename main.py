fef24d180af882d119f863e8086dc96cdac98253
Soulbolt <soulbolt@gmail.com>
2023-12-22 16:06:18 -0700

    Added search password function, implemented try exception to handle if file/password does not exist, notify user with respective data info/notifications.

--- main.py
+++ main.py
@@ -21,6 +21,21 @@ def generate_password():
     window.clipboard_append(password)
 
 
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def search():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            # read old data
+            data = json.load(data_file)
+            print(data)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            messagebox.showinfo(title=website, message=f"Email: {data[website]["email"]}\nPassword: {data[website]["password"]}")
+        else:
+            messagebox.showinfo(title="Unknown", message=f"No {website} exist in file.")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def clear_entries():
     website_entry.delete(0, END)
@@ -76,19 +91,21 @@ password_label.grid(row=3, column=0)
 
 # Entries
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=55)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "soulbolt@gmail.com")
-password_entry = Entry(width=21)
+password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button = Button(text="Generate Password", width=16, command=generate_password)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
+add_button = Button(text="Add", width=47, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=16, command=search)
+search_button.grid(row=1, column=2)
 
 
 
