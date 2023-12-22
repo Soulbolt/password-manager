67cece61ac7eaf5ba5faa0e183769896b5add9e5
Soulbolt <soulbolt@gmail.com>
2023-12-21 23:56:57 -0700

    Implemented functions to write entrys to data.txt file, validate fields have been populated, confirm entries before saving, generate random password, and clear fields after saving.

--- main.py
+++ main.py
@@ -1,34 +1,77 @@
 from tkinter import *
+from tkinter import messagebox  #  Module, not a class
+from random import choice, randint, shuffle
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def clear_entries():
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
 
+
+def save():
+    website = website_entry.get()
+    email_username = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="oops", message="Please make sure there are no fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \vEmail: {email_username} Password: {password}\n Is it ok to save?")
+        
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(f"{website} | {email_username} | {password}\n")
+            clear_entries()
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=0)
 
+# Labels
 website_label = Label(text="Website:")
-website_label.grid(column=0, row=2)
-website_input = Entry(width=60)
-website_input.grid(column=1, row=2)
+website_label.grid(row=1, column=0)
 email_username_label = Label(text="Email/Username:")
-email_username_label.grid(column=0, row=3)
-email_entry = Entry(width=60)
-email_entry.grid(column=1, row=3)
+email_username_label.grid(row=2, column=0)
 password_label = Label(text="Password")
-password_label.grid(column=0, row=4)
-password_entry = Entry(width=32)
-password_entry.grid(column=1, row=4)
-generate_password_button = Button(text="Generate Password",)
-generate_password_button.grid(column=1.5, row=4)
-add_button = Button(text="Add", width=60)
-add_button.grid(column=1, row=5)
+password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "soulbolt@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
 
